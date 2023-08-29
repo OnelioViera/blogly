e871cc25d2ec30163ab76e1c388cafce9485281e
Onelio Viera <ojvwebdesign@gmail.com>
2023-08-29 18:10:09 -0400

    update file

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'ihaveasecret'
 
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
@@ -19,10 +20,13 @@ def root():
     posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
     return render_template('main-homepage.html', posts=posts)
   
-# @app.route("/signIn")
-# def signed_in():
-#     return render_template("signIn.html")
-  
+#####################################################################
+# Login routes
+
+@app.route("/signIn")
+def signed_in():
+    return render_template("signIn.html")
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -150,17 +154,91 @@ def posts_new_form(user_id):
 
     user = User.query.get_or_404(user_id)
     return render_template('posts/new.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def posts_new(user_id):
+    """Handle form submission for creating a new post for a specific user"""
+
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['title'],
+                    content=request.form['content'],
+                    user=user)
+
+    db.session.add(new_post)
+    db.session.commit()
+    flash(f"Post '{new_post.title}' added.")
+
+    return redirect(f"/users/{user_id}")
+
+
+@app.route('/posts/<int:post_id>')
+def posts_show(post_id):
+    """Show a page with info on a specific post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/show.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def posts_edit(post_id):
+    """Show a form to edit an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/edit.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def posts_update(post_id):
+    """Handle form submission for updating an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' edited.")
+
+    return redirect(f"/users/{post.user_id}")
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def posts_destroy(post_id):
+    """Handle form submission for deleting an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+    flash(f"Post '{post.title} deleted.")
+
+    return redirect(f"/users/{post.user_id}")
   
   
 #####################################################################
 # Datbase UI route
 
-@app.route('/db')
+@app.route('/dashboard')
 def db_debug():
     """Show a page with info on all users"""
 
     users = User.query.order_by(User.last_name, User.first_name).all()
-    return render_template('db.html', users=users)
+    return render_template('dashboard.html', users=users)
+  
+@app.route('/users-db')
+def usersdb():
+    """Show a page with info on all users"""
+
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('users-db.html', users=users)
+  
+@app.route('/posts-db')
+def postsdb():
+    """Show a page with info on all users"""
+
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('posts-db.html', users=users)
   
   
 if __name__ == '__main__':
